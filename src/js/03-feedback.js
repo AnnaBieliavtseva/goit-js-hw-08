a581a898406379138212291e88bc018b23c66c4b
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2022-08-20 12:43:17 +0200

    finish 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+const formRef = document.querySelector('.feedback-form');
+const submitRef = document.querySelector('button[type="submit"]');
 
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'form-data';
+
+const formData = {};
+
+populateUserData();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  console.log(formData);
+
+  localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateUserData() {
+  const savedData = localStorage.getItem('STORAGE_KEY');
+  const parsedSavedData = JSON.parse(savedData);
+  if (savedData) {
+    console.log(parsedSavedData);
+    formRef.value = parsedSavedData;
+  }
+}
